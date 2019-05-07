48c391eeb2baa530ddbfe17498b3e0749b5affb1
Mextrin <seba3552@hotmail.com>
2019-05-07 11:44:18 +0200

    Uploaded player controller and movement

---
+++ Assets/InputHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    static List<int> assignedControllers = new List<int>();
+    public string[] controllers;
+
+    public int controllerID = 0;
+
+    public bool ConnectedController { get { return controllerID > 0; } }
+    public string Horizontal { get { return "Controller" + controllerID + "Horizontal"; } }
+    public string Vertical { get { return "Controller" + controllerID + "Vertical"; } }
+    public string Jump { get { return "Controller" + controllerID + "Jump"; } }
+    public string Action { get { return "Controller" + controllerID + "Action"; } }
+
+    public static void FlushControllers()
+    {
+        assignedControllers.Clear();
+    }
+
+    void Start()
+    {
+        controllers = Input.GetJoystickNames();
+        //Makes sure player1 (keyboard) always has control in the game
+        if (!assignedControllers.Contains(1))
+        {
+            assignedControllers.Add(1);
+            controllerID = 1;
+        }
+    }
+
+    void Update()
+    {
+        if (controllerID <= 0)
+        {
+            //See if any controller activates
+            for (int i = 1; i <= 2; i++)
+            {
+                if (!assignedControllers.Contains(i) && Input.GetButtonDown("Controller" + i + "Action"))
+                {
+                    controllerID = i;
+                    assignedControllers.Add(i);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(InputHandler))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    InputHandler input;
+
+    [Header("Movement")]
+    private Rigidbody rigidbody;
+
+    [SerializeField]
+    float speed = 5.0f;
+    [SerializeField]
+    float hitRange = 5.0f;
+    [SerializeField]
+    int hitWidth = 9;
+    [SerializeField]
+    float pushForce = 10.0f;
+    [SerializeField]
+    float pushForceZ = 10.0f;
+    [SerializeField]
+    float rotationSpeed = 0.2f;
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        input = GetComponent<InputHandler>();
+    }
+
+    float lerpTime;
+    Vector3 lerpTarget;
+    private void Update()
+    {
+        if (lerpTime > 0)
+        {
+            lerpTime -= Time.deltaTime / 0.5f;
+            transform.position = Vector3.Lerp(transform.position, lerpTarget, Mathf.Clamp01(1 - lerpTime));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Moving();
+        Fire();
+
+
+    }
+
+    void Moving()
+    {
+        if (input.ConnectedController)
+        {
+            //Move
+            Vector3 moveDirection = new Vector3(Input.GetAxisRaw(input.Horizontal), 0.0f, -Input.GetAxisRaw(input.Vertical));
+            rigidbody.velocity = moveDirection * 100 * speed * Time.deltaTime;
+
+            //Turn character
+            if (moveDirection.sqrMagnitude > 0.0f)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection, Vector3.up), rotationSpeed);
+            }
+        }
+    }
+
+    void Fire()
+    {
+        if (input.ConnectedController)
+        {
+            if (Input.GetButtonDown(input.Action))
+            {
+
+                //Debug.Log(input.controllerID + " Pressed X");
+                RaycastHit hit;
+                Vector3 origin = transform.position;
+                int angle = -50;
+
+                for (int i = 0; i < hitWidth; i++)
+                {
+                    Vector3 forwardDirection = Quaternion.Euler(0, angle, 0) * transform.forward;
+                    angle += 20;
+
+                    Debug.DrawRay(origin, forwardDirection * hitRange, Color.blue, 3f);
+                    if (Physics.Raycast(origin, forwardDirection, out hit, hitRange))
+                    {
+                        //Debug.Log(hit);
+                        //this pushes a box
+                        if (hit.collider.tag == "Box")
+                        {
+                            //hit.collider.gameObject.GetComponent<GetPushed>().AddForce(forwardDirection);
+                        }
+                        //This pushed the player
+                        if (hit.collider.tag == "Player" && hit.collider != this)
+                        {
+                            //hit.collider.gameObject.GetComponent<PlayerController>().GetPushed(forwardDirection);
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+
+    //The function to get pushed
+    void GetPushed(Vector3 direction)
+    {
+        lerpTime = 1;
+        lerpTarget = transform.position + (direction * 10);
+        //rigidbody.AddForce(new Vector3(direction.x * pushForce, pushForceZ, direction.y * pushForce), ForceMode.Impulse);
+    }
+}
